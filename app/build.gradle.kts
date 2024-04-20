bda5bf3e5cca456c6697f41b3b6c546d298aec23
georgievartemv <georgievartemv@yandex.com>
2024-04-20 21:18:12 +0300

    v 1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,9 +33,25 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
+
+    implementation ("com.squareup.retrofit2:converter-gson:2.1.0")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.github.bumptech.glide:glide:5.0.0-rc01")
+    implementation ("androidx.core:core-splashscreen:1.0.1")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation ("com.squareup.retrofit2:adapter-rxjava2:2.7.1")
+    implementation ("com.facebook.shimmer:shimmer:0.5.0@aar")
+    val room_version = "2.6.1"
+    implementation("androidx.room:room-rxjava2:$room_version")
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
--- app/src/main/java/com/example/kinopoiskpetproject/DetailsFragment.kt
+++ app/src/main/java/com/example/kinopoiskpetproject/DetailsFragment.kt
@@ -6,13 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class CoreFragment : Fragment() {
+class DetailsFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_core, container, false)
+        return inflater.inflate(R.layout.fragment_details, container, false)
     }
 }
